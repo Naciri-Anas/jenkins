5c6165ee451b31d41046b5dba75b66243a94ed31
Anas Naciri <110792944+Naciri-Anas@users.noreply.github.com>
2024-01-08 12:07:36 +0100

    Update HelloController.java

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "popopopo 1dsds11d:)";
+        return "popopopo 1dsdsddsdss11d:)";
     }
 
     @GetMapping("/user")
